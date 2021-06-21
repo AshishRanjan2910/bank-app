9a430021912d2a685464dc131d03d23e137c0f7b
AshishRanjan2910 <67757899+AshishRanjan2910@users.noreply.github.com>
2021-06-21 16:18:52 +0530

    skeleton of JS done

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const main = document.getElementById('main');
+const btnAddUser = document.getElementById('add-user');
+const btnDouble = document.getElementById('double');
+const btnFilter = document.getElementById('filter-rich');
+const btnTotal = document.getElementById('total');
+
+//add user
+
+//fetch a random user from random user
+
+//update DOM
+
+
+
