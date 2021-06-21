84311090210e098eea1beb791f5ee1e3ba45efaf
AshishRanjan2910 <67757899+AshishRanjan2910@users.noreply.github.com>
2021-06-21 17:12:53 +0530

    add user - using - forEach (JS)

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const main = document.getElementById('main');
 const btnAddUser = document.getElementById('add-user');
 const btnDouble = document.getElementById('double');
@@ -8,9 +6,63 @@ const btnTotal = document.getElementById('total');
 
 //add user
 
+let data = [];
+
 //fetch a random user from random user
 
-//update DOM
+let data_box = [];
+
+const getRandomUser = async function(){
+    const res = await fetch('https://randomuser.me/api/');
+    const data = await res.json();
+    const user = data.results[0];
+
+    // console.log(user);
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        balance: Math.floor(Math.random()* 100000),
+
+    }
+
+    // console.log(newUser);
+    addData(newUser);
+}
 
 
+// Function addData
+
+const addData = function(obj){
+    data_box.push(obj);
+    updateDOM();
+}
+
+//function update DOM
+
+const updateDOM = function(provideData = data_box){
+    //clear main
+    main.innerHTML = '<h2><strong>Name</strong> Balance</h2>';
+    provideData.forEach(item =>{
+        const element = document.createElement('div');
+        element.classList.add('users');
+        element.innerHTML = `<strong>${item.name}</strong>₹${formatToCurrency(item.balance)}`;
+        main.appendChild(element);
+    });
+}
+
+function formatToCurrency(amount){
+    return amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&, ');
+}
+
+
+// call random users 
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+
+////Event Listener
+
+// add new user using button
+btnAddUser.addEventListener('click', getRandomUser);
 
